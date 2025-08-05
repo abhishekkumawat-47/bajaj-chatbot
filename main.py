12b3553fa45c3022274c857299c8acf3b6172c56
vidhan66 <vidhanbansal66@gmail.com>
2025-08-05 13:20:37 +0530

    Added file input and chat history support

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ not just keyword matching.
 - Evaluate the query using the retrieved clauses and return a factual response.
 
 Guidelines:
-- Never make assumptions not supported by documents.
+- Only tell the details mentioned in the documents , don't add additional details that are not mentioned in the documents.
 - If required info is missing, return `"decision": "needs_clarification"` and explain what’s needed.
 - If no relevant clause is found, say so and show the closest matching content.
 - Be explainable, traceable, and cautious. Don’t hallucinate.
@@ -43,38 +43,40 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.4
 )
 
+
 loader1 = PyPDFLoader(r"C:\Python\Pycharm\Project\bajaj chatbot\Sample1.pdf")
 loader2 = PyPDFLoader(r"C:\Python\Pycharm\Project\bajaj chatbot\Sample2.pdf")
 loader3 = PyPDFLoader(r"C:\Python\Pycharm\Project\bajaj chatbot\Sample3.pdf")
-loader4 = PyPDFLoader(r"C:\Python\Pycharm\Project\bajaj chatbot\Sample4.pdf")
-loader5 = PyPDFLoader(r"C:\Python\Pycharm\Project\bajaj chatbot\Sample5.pdf")
+
 docs1 = loader1.load()
 docs2 = loader2.load()
 docs3 = loader3.load()
-docs4 = loader4.load()
-docs5 = loader5.load()
-docs = docs1 + docs2 + docs3 + docs4 + docs5
 
-embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(splits, embeddings)
-retriever = vectorstore.as_retriever(
-    search_type="mmr",
-    search_kwargs={"k": 10, "fetch_k": 30}
-)
+docs = docs1 + docs2 + docs3
 
-qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=retriever,
-        chain_type="stuff",
-        return_source_documents=False,
-        verbose=True
+def retriever_from_docs(docs):
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(splits, embeddings)
+    retriever = vectorstore.as_retriever(
+        search_type="mmr",
+        search_kwargs={"k": 10, "fetch_k": 30}
     )
-@tool
-def ask_query(query: str) -> dict:
-    """Search documents and return a structured decision based on the policy."""
+    qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            retriever=retriever,
+            chain_type="stuff",
+            return_source_documents=False,
+            verbose=True
+        )
+    return qa_chain
 
+@tool
+def ask_query(query: str,user_docs=None) -> dict:
+    """Search documents and return a structured decision based on the policy."""
+    combined_docs = docs + user_docs if user_docs else docs
+    qa_chain = retriever_from_docs(combined_docs)
     response = qa_chain.invoke({"query": query})
     return response
 @tool
